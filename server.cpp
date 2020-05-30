12d9651d790f21eb4587e8820e0116578ea0058a
Nurseiit Abdimomyn <nurs@unist.ac.kr>
2020-05-30 20:44:26 +0600

    ✨ Server mvp

--- server.cpp
+++ server.cpp
@@ -1,34 +1,162 @@
 #include <asio.hpp>
-#include <ctime>
 #include <iostream>
+#include <istream>
+#include <map>
+#include <ostream>
 #include <string>
-
-#define PORT 80
 
 using asio::ip::tcp;
 
-std::string build_message() { return "Hello from Server!"; }
+class Session;
 
-int main() {
-  try {
-    asio::io_context io_context;
+class Headers {
+  std::string method;
+  std::string url;
+  std::string version;
 
-    // Listen on 0.0.0.0:PORT
-    tcp::acceptor acceptor(io_context, tcp::endpoint(tcp::v4(), PORT));
+  std::map<std::string, std::string> headers_map;
 
-    while (true) {
-      tcp::socket socket(io_context);
-      acceptor.accept(socket);
-
-      std::string message = build_message();
-
-      asio::error_code error;
-      asio::write(socket, asio::buffer(message), error);
-      std::cerr << error.value() << std::endl;
+public:
+  std::string get_response() {
+    std::stringstream response_stream;
+    // TODO read from files
+    if (url == "/") {
+      std::string content = "<html><body><h1>Hello World</h1><p>This is a web "
+                            "server in using asio</p></body></html>";
+      response_stream << "HTTP/1.1 200 OK" << std::endl;
+      response_stream << "content-type: text/html" << std::endl;
+      response_stream << "content-length: " << content.length() << std::endl;
+      response_stream << std::endl;
+      response_stream << content;
+    } else {
+      std::string content = "<html><body><h1>404 Not Found</h1></body></html>";
+      response_stream << "HTTP/1.1 404 Not Found" << std::endl;
+      response_stream << "content-type: text/html" << std::endl;
+      response_stream << "content-length: " << content.length() << std::endl;
+      response_stream << std::endl;
+      response_stream << content;
     }
-  } catch (std::exception &e) {
-    std::cerr << e.what() << std::endl;
+    return response_stream.str();
   }
 
-  return 0;
+  int content_length() {
+    if (headers_map.count("content-length")) {
+      std::stringstream content_length_stream(headers_map["content-length"]);
+      int content_length;
+      content_length_stream >> content_length;
+      return content_length;
+    }
+    return 0;
+  }
+
+  void read_header(std::string line) {
+    std::stringstream header_stream(line);
+    std::string header_name;
+    std::getline(header_stream, header_name, ':');
+
+    std::string header_value;
+    std::getline(header_stream, header_value);
+    headers_map[header_name] = header_value;
+  }
+
+  void read_request_line(std::string line) {
+    std::stringstream request_stream(line);
+    request_stream >> method;
+    request_stream >> url;
+    request_stream >> version;
+
+    std::cout << "🙈 Requested: " << url << std::endl;
+  }
+};
+
+class Session {
+  asio::streambuf buf;
+  Headers headers;
+
+  static void read_body(std::shared_ptr<Session> ptr) {
+    int nbuffer = 6969;
+    std::shared_ptr<std::vector<char>> bufptr =
+        std::make_shared<std::vector<char>>(nbuffer);
+    asio::async_read(ptr->socket, asio::buffer(*bufptr, nbuffer),
+                     [ptr](const asio::error_code &e, std::size_t s) {});
+  }
+
+  static void read_next_line(std::shared_ptr<Session> ptr) {
+    asio::async_read_until(
+        ptr->socket, ptr->buf, '\r',
+        [ptr](const asio::error_code &e, std::size_t s) {
+          std::string line, _;
+          std::istream stream{&ptr->buf};
+          std::getline(stream, line, '\r');
+          std::getline(stream, _, '\n');
+          ptr->headers.read_header(line);
+
+          if (line.length() == 0) {
+            if (ptr->headers.content_length() == 0) {
+              std::shared_ptr<std::string> str =
+                  std::make_shared<std::string>(ptr->headers.get_response());
+              asio::async_write(
+                  ptr->socket, asio::buffer(str->c_str(), str->length()),
+                  [ptr, str](const asio::error_code &e, std::size_t s) {
+                    std::cout << "👌 done" << std::endl;
+                  });
+            } else {
+              ptr->read_body(ptr);
+            }
+          } else {
+            ptr->read_next_line(ptr);
+          }
+        });
+  }
+
+  static void read_first_line(std::shared_ptr<Session> ptr) {
+    asio::async_read_until(ptr->socket, ptr->buf, '\r',
+                           [ptr](const asio::error_code &e, std::size_t s) {
+                             std::string line, _;
+                             std::istream stream{&ptr->buf};
+                             std::getline(stream, line, '\r');
+                             std::getline(stream, _, '\n');
+                             ptr->headers.read_request_line(line);
+                             ptr->read_next_line(ptr);
+                           });
+  }
+
+public:
+  tcp::socket socket;
+
+  Session(asio::io_service &io_service) : socket(io_service) {}
+
+  static void start(std::shared_ptr<Session> ptr) { read_first_line(ptr); }
+};
+
+void accept_and_run(tcp::acceptor &acceptor, asio::io_service &io_service) {
+  std::shared_ptr<Session> sesh = std::make_shared<Session>(io_service);
+  acceptor.async_accept(
+      sesh->socket,
+      [sesh, &acceptor, &io_service](const asio::error_code &accept_error) {
+        accept_and_run(acceptor, io_service);
+        if (!accept_error) {
+          Session::start(sesh);
+        }
+      });
+}
+
+int main(int argc, const char *argv[]) {
+  if (argc != 2) {
+    std::cout << "Usage: " << argv[0] << " <port>\n";
+    std::cout << "Examples:\n";
+    std::cout << "  " << argv[0] << " 8080\n";
+    return EXIT_FAILURE;
+  }
+
+  int PORT = std::atoi(argv[1]);
+
+  asio::io_service io_service;
+  tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), PORT));
+
+  acceptor.listen();
+  accept_and_run(acceptor, io_service);
+
+  io_service.run();
+  return EXIT_SUCCESS;
 }
